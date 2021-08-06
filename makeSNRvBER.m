0b51b703bc2e3f0cbbea1b7a2f727ede4c57b1bf
Alex Salois <asalois.scholar@gmail.com>
2021-08-06 10:08:21 -0600

    256 128 for dfe is the best

--- makeSNRvBER.m
+++ makeSNRvBER.m
@@ -5,21 +5,23 @@ clear; clc; close all;
 rng(123)
 tic
 fiberLength = 13;
-iters = 100;
+iters = 10000;
 [berA, x] = lmsSNRvBER([2 2^10 0.01],fiberLength,iters);
 toc
-[berC, ~] = dfeSNRvBER([2 1 2^7],fiberLength,iters);
+[berB, ~] = dfeSNRvBER([2 1 2^7],fiberLength,iters);
 toc
-berB = [berA; berC];
+[berC, ~] = dfeSNRvBER([2 1 2^8],fiberLength,iters);
+toc
+berD = [berA; berB; berC;];
 
 
 %%
 figure()
-semilogy(x,berB','-*')
+semilogy(x,berD','-*')
 xlabel('SNR [dB]')
 ylabel('BER')
-legend('NO EQ','LMS','DFE','Location','southwest')
+legend('NO EQ','LMS','DFE 128','DFE 256','Location','southwest')
 title('EQs for 13 m POF')
-% saveName = sprintf('SNRvBER_taps_%03d.png', i+1);
-% saveas(gcf,saveName)
+saveas(gcf,'Eqsfor13mPOF.png')
+save('Eqsfor13mPOF')
 toc
