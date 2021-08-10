d90bf81d7c03e6e5dc779cdfa0cfef9854148bff
Alexander Salois <asalois.scholar@gmail.com>
2021-08-10 14:16:22 -0600

    close previous parpoll

---
+++ runSearch.m
@@ -0,0 +1,58 @@
+function runSearch(i)
+% Montana State University
+% Electrical & Computer Engineering Department
+% Created by Alexander Salois
+
+tic
+delete(gcp('nocreate'))
+maxNumCompThreads(8)
+parpool('local',8)
+
+fiberLength = 13;
+
+transferFs = {...
+    'compet'
+    'elliotsig'
+    'hardlim'
+    'hardlims'
+    'logsig'
+    'netinv'
+    'poslin'
+    'purelin'
+    'radbas'
+    'radbasn'
+    'satlin'
+    'satlins'
+    'softmax'
+    'tansig'
+    'tribas'
+    };
+
+trainFs = {...
+    'trainlm'
+    'trainbr'
+    'trainbfg'
+    'trainrp'
+    'trainscg'
+    'traincgb'
+    'traincgf'
+    'traincgp'
+    'trainoss'
+    'traingdx'
+    'traingdm'
+    'traingd'
+    };
+
+indx = combvec(1:12,1:15);
+i
+size(indx)
+train = char(trainFs(indx(1,i)));
+trans = char(transferFs(indx(2,i)));
+[berR] = annSearch(40,4000,train,trans,fiberLength);
+berR
+saveName = sprintf('search_%03d',i);
+save(saveName,'berR','train','trans');
+toc
+
+end
+
