cef4aee9a6463f80c7734684012b23f08f507e79
Alexander Salois <asalois.scholar@gmail.com>
2021-08-11 21:17:33 -0600

    loop through all fiber lengths

--- makeSNRvBER.m
+++ makeSNRvBER.m
@@ -4,31 +4,37 @@
 clear; clc; close all;
 rng(123)
 tic
-fiberLength = 13;
-iters = 100;
-[berA, x] = lmsSNRvBER([2 2^10 0.01],fiberLength,50);
-toc
-[berB, ~] = dfeSNRvBER([2 1 2^7],fiberLength,iters);
-toc
-[berC, ~] = dfeSNRvBER([2 1 2^8],fiberLength,iters);
-toc
+for k = 1:20
+	fiberLength = k
+	iters = 400;
+	[berA, x] = lmsSNRvBER([2 2^10 0.01],fiberLength,iters);
+	toc
+	[berB, ~] = dfeSNRvBER([2 1 2^7],fiberLength,iters);
+	toc
+	[berC, ~] = dfeSNRvBER([2 1 2^8],fiberLength,iters);
+	toc
 
-%%
-tic
-[berE, ~] = annSNRvBER(40,(2^17 - 30),5000,fiberLength,1);
-toc
-[berF, ~] = annlSNRvBER(40,(2^17 - 30),5000,fiberLength,1);
-toc
-berD = [berA; berB; berC; berE; berF];
+	%
+	tic
+	%[berE, ~] = annSNRvBER(40,(2^17 - 30),5000,fiberLength,1);
+	toc
+	[berF, ~] = annlSNRvBER(40,(2^17 - 30),5000,fiberLength,1);
+	toc
+	%berD = [berA; berB; berC; berE; berF];
+	berD = [berA; berB; berC; berF];
 
 
-%%
-figure()
-semilogy(x,berD','-*')
-xlabel('SNR [dB]')
-ylabel('BER')
-legend('NO EQ','LMS','DFE 128','DFE 256','ANN','Linear ANN','Location','southwest')
-title('EQs for 13 m POF')
-saveas(gcf,'Eqsfor13mPOF_2.png')
-save('Eqsfor13mPOF_2')
-% toc
+	%
+	figure()
+	semilogy(x,berD','-*')
+	xlabel('SNR [dB]')
+	ylabel('BER')
+	legend('NO EQ','LMS','DFE Train 128','DFE Train 256','Linear ANN','Location','southwest')
+	titleName = sprintf('EQs for %d m of POF',fiberLength);
+	title(titleName)
+	saveFigureName = sprintf('Eqsfor%02dmPOF.png',fiberLength);
+	saveas(gcf,saveFigureName)
+	saveName = sprintf('Eqsfor%02dmPOF',fiberLength);
+	save(saveName)
+	toc
+end
