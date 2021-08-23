8e74c03c3289b0be38246433cd7ea1ca40ad5a70
Alexander Salois <asalois.scholar@gmail.com>
2021-08-23 15:35:34 -0600

    generated launch

---
+++ launch_nns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+sbatch cv01.slurm
+sbatch test01.slurm
+sbatch cv02.slurm
+sbatch test02.slurm
+sbatch cv03.slurm
+sbatch test03.slurm
+sbatch cv04.slurm
+sbatch test04.slurm
+sbatch cv05.slurm
+sbatch test05.slurm
