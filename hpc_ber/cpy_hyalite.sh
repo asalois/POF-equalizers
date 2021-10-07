23690383734bc38a0108005cc68fdda2ce5923ce
Alexander Salois <asalois.scholar@gmail.com>
2021-10-06 21:05:09 -0600

    run things scripts

---
+++ hpc_ber/cpy_hyalite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+scp scan_dfe.m v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/dfe_scan_new/
+
--- hpc_ber/run_get_ber.sh
+++ hpc_ber/run_get_ber.sh
@@ -2,4 +2,5 @@
 # script to organize the output and make new dir
 #matlab -nodesktop -nodisplay -nosplash -singleCompThread -r "addpath('/home/alexandersalois/Documents/POF-equalizers');get_ber;exit;"
 #matlab -nodesktop -nodisplay -nosplash -r "make_ber_eqs;exit;"
-matlab -nodesktop -nodisplay -nosplash -r "scan_lms(100);exit;"
+#matlab -nodesktop -nodisplay -nosplash -r "scan_lms(100);exit;"
+matlab -nodesktop -nodisplay -nosplash -r "scan_dfe(1000);exit;"
