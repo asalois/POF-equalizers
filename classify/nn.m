80e01f8dde9ccc853e7843790bc646b23d3b6fc9
Alexander Salois <asalois.scholar@gmail.com>
2022-02-21 15:56:04 -0700

    adjusted how BER works

--- classify/nn.m
+++ classify/nn.m
@@ -7,12 +7,12 @@ clear; clc; close all;
 tic
 graph = zeros(31,2);
 neurons = 100;
-samples = 1;
+samples = 2;
 name = 'logsig';
-for snr = 5:40
+for snr = 5:35
 	%dirName = "/home/alexandersalois/DataDrive/optSimData/05_samples/";
     %dirName = "D:/OneDrive - Montana State University/03_samples/08_signals/";
-    dirName = "D:/OneDrive - Montana State University/TF_data/01_samples/08_signals/";
+    dirName = "D:/OneDrive - Montana State University/TF_data/02_samples/64_signals/";
 	matName = sprintf('testDataSnr%02d',snr) % test
 	loadName = dirName + matName;
 	load(loadName)
@@ -24,30 +24,32 @@ for snr = 5:40
 	net.trainParam.showCommandLine = false;
 %     net.trainParam.min_grad = 10^(-7);
 %     net.trainParam.max_fail = 10;
-	net = train(net,trainIn,trainTarget,'useGPU','yes'); % use GPU
-% 	net = train(net,testTrainIn,testTrainTarget,'useParallel','yes');
+%	net = train(net,trainIn,trainTarget,'useGPU','yes'); % use GPU
+	net = train(net,testTrainIn,testTrainTarget,'useParallel','yes');
 	y = net(testIn);
 	perf = perform(net,testTarget,y)
 
 	%%
-	pred = zeros(1,length(y));
-	for i = 1:length(y)
-	    if y(1,i) > 0.9
-		pred(i)=3;
-	    elseif y(2,i) > 0.9
-		pred(i)=1;
-	    elseif y(3,i) > 0.9
-		pred(i)=-1;
-	    elseif y(4,i) > 0.9
-		pred(i)=-3;
-	    end
-	end
+	[~.maxIndx] = max(y,[],2);
+	predSeq = zeros(1,length(y));
+        for z = 1:length(maxIndx)
+              if maxIndx(z) == 1
+                  predSeq(z)=3;
+              elseif maxIndx(z) == 2
+                  predSeq(z)=1;
+              elseif maxIndx(z) == 3
+                  predSeq(z)=-1;
+             elseif maxIndx(z) == 4
+                  predSeq(z)=-3;
+              end
+        end
+
 	%%
 	testSeq = (testSeq - 0.5 ) * 6;
 	seq = (testIn(1,:) - 0.5 ) * 6;
 	x = pamdemod(testSeq',4);
 	y_a = pamdemod(seq',4);
-	y_b = pamdemod(pred',4);
+	y_b = pamdemod(predSeq',4);
 
 
 	%%
