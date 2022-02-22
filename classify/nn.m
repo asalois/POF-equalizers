552807f225f1a6bcbb02a955c2e9f019535e36ca
Alexander Salois <asalois.scholar@gmail.com>
2022-02-22 15:14:00 -0700

    changed to dynamically change the load file and included GPU pool mode

--- classify/nn.m
+++ classify/nn.m
@@ -7,30 +7,34 @@ clear; clc; close all;
 tic
 graph = zeros(31,2);
 neurons = 100;
-samples = 2;
+samples = 7;
+signals = 8;
 name = 'logsig';
+dirName = sprintf(...
+'/home/alexandersalois/DataDrive/TF_data/%02d_samples/%02d_signals/',...
+samples,signals)
+%dirName = "D:/OneDrive - Montana State University/03_samples/08_signals/";
+%dirName = "D:/OneDrive - Montana State University/TF_data/02_samples/64_signals/";
 for snr = 5:35
-	%dirName = "/home/alexandersalois/DataDrive/optSimData/05_samples/";
-    %dirName = "D:/OneDrive - Montana State University/03_samples/08_signals/";
-    dirName = "D:/OneDrive - Montana State University/TF_data/02_samples/64_signals/";
+	% load in correct data for SNR
 	matName = sprintf('testDataSnr%02d',snr) % test
-	loadName = dirName + matName;
+	loadName = strcat(dirName,matName);
 	load(loadName)
 
-	%%
+	% make and train NN
 	net = patternnet(neurons,'trainscg');
 	net.layers{1}.transferFcn = name;
-	net.trainParam.showWindow = true;
-	net.trainParam.showCommandLine = false;
-%     net.trainParam.min_grad = 10^(-7);
-%     net.trainParam.max_fail = 10;
-%	net = train(net,trainIn,trainTarget,'useGPU','yes'); % use GPU
-	net = train(net,testTrainIn,testTrainTarget,'useParallel','yes');
+	net.trainParam.showWindow = false;
+	net.trainParam.showCommandLine = true;
+	net = train(net,[trainIn valIn],[trainTarget valTarget],'useGPU','yes'); % use GPU
+%	net = train(net,[trainIn valIn],[trainTarget valTarget],'useParallel','yes'); % use CPU Pool
+%	net = train(net,[trainIn valIn],[trainTarget valTarget],...
+%	'useParallel','yes','useGPU','only'); % use CPU Pool for each GPU
 	y = net(testIn);
 	perf = perform(net,testTarget,y)
 
 	%%
-	[~.maxIndx] = max(y,[],2);
+	[~,maxIndx] = max(y,[],1);
 	predSeq = zeros(1,length(y));
         for z = 1:length(maxIndx)
               if maxIndx(z) == 1
