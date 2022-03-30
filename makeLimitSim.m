d22f4b3dbccb7fba42a25870680a1294fa2639f0
Alexander Salois <asalois.scholar@gmail.com>
2022-03-30 14:48:35 -0600

    move bitsIn outside loop

--- makeLimitSim.m
+++ makeLimitSim.m
@@ -1,13 +1,13 @@
 tic
-ns= 2^28
+ns= 2^26
 M = 4
 x = randi(M-1,ns,1);
 sig = pammod(x,M);
+bitsIn = pamdemod(sig,M);
 ber = zeros(1,31);
 errs = zeros(1,31);
 for snr = 5:35
     selectOutSNR =  awgn(sig,snr,'measured');
-    bitsIn = pamdemod(sig,M);
     bitsOut = pamdemod(selectOutSNR,M);
     % get BER
     [errs(1,snr-4),ber(1,snr-4)] = biterr(bitsIn,bitsOut);
